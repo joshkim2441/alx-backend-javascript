f2dc97387fced390cf822420c0ffe2e743f0244f
joshkim2441 <joshkim2441@gmail.com>
2024-07-02 17:32:53 +0300

    Task 1 correction

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -8,11 +8,11 @@
  * @returns an array of IDs from a list of objects
  */
 
-export default function getListStudentIds(arr) {
-    if (!Array.isArray(arr)) {
-        return []; /* Return an empty array if
+export default function getListStudentIds(students) {
+    if (students instanceof Array) {
+        return students.map((student) => student.id); /* Return an empty array if
         argument is not an array */
     }
-    return arr.map(obj => obj.id); /* Use map to extract 'id' property from
+    return []; /* Use map to extract 'id' property from
     each object */
 }
